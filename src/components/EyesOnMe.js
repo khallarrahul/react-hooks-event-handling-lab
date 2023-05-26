f52ae1379c57ad0389642d1b30093231ea6acdd4
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-05-26 13:53:34 -0700

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  function handleFocus() {
+    console.log("Good!");
+  }
+  function handleBlur() {
+    console.log("Hey! Eyes on me!");
+  }
+  return (
+    <main>
+      <button onFocus={handleFocus} onBlur={handleBlur}>
+        Eyes on me
+      </button>
+    </main>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleChange() {
+    console.log("Entering password...");
+  }
+  return (
+    <main>
+      <input type="password" onChange={handleChange} />
+    </main>
+  );
 }
 
 export default Keypad;
